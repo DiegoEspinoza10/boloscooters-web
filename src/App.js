0fe9606a5d6eb65b705dbf244971d5d97ac4d6b8
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2023-04-14 20:48:04 -0600

    Footer commit before installing fortawesome icons for social media

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, BrowserRouter,Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
-
+import Footer
+ from './components/Footer';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" exact Component={Home}/>
       </Routes>
+      <Footer />
       </BrowserRouter>
     </>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <section className='footer-subscription'>
+            <h1 className='footer-subscription-heading'>
+                Ubicacion
+            </h1>
+            <p className='footer-subscription-text'>
+                Costa Rica, Alajuela, Alajuela
+            </p>
+            <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.001569254865!2d-84.22687905742931!3d10.016728172749207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0f9cdacd0a197%3A0x890bdac95730c3a2!2sOficentro%20FES!5e0!3m2!1ses!2scr!4v1681505386420!5m2!1ses!2scr" 
+            style={{ border: 0 }} 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class='footer-links'>
+            <div className='footer-link-wrapper'>
+                <div class='footer-link-items'>
+                    <h2>Redes Sociales</h2>
+                    <a href='https://www.facebook.com/Boloscooters'>Facebook</a>
+                    <a href='https://www.instagram.com/boloscooters/'>Instagram</a>
+                </div>
+            </div>
+        </div>
+        </section>
+    </div>
+  )
+}
+
+export default Footer
